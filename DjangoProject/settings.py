23cce54dd137e8aa11cebfe722c696d424489866
Florian <fr@ruffner-beratung.de>
2025-04-18 19:53:51 +0200

    db setup

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -90,8 +90,15 @@ WSGI_APPLICATION = 'DjangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crm_db',
+        'USER': 'admin1',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        }
     }
 }
 
--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -1,10 +1,12 @@
 # DjangoProject/urls.py
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/ai/', include('ai_assistant.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api-auth/', include('rest_framework.urls')),
 ]
